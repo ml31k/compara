5fc7b3c5b02989a89fe19f91cd753f755dabb458
Minggao Liang <mliang@qlogin22.ccm.sickkids.ca>
2021-09-07 17:51:39 -0400

    Fixed bug in comparaR.R where only the first row was labeled as 'D'

--- comparaR.R
+++ comparaR.R
@@ -9,7 +9,7 @@ stitchbp = 2 ## Compara often reports regions with small gaps. Regions separated
 
 args <- commandArgs(trailingOnly=TRUE)
 
-##args <- c("~/mdwilson/mliang/projects/ChIP_exo_20210722/results/ChExMix_peaks/compara/CEBPA/rscript_instructions.txt", "S")
+args <- c("~/mdwilson/mliang/projects/ChIP_exo_20210722/results/ChExMix_peaks/compara/CEBPA/rscript_instructions.txt", "S")
 
 if(length(args)==0){
     print(helpmsg)
@@ -123,7 +123,9 @@ matched.compara.list <- lapply(species.list, function(origin.species){ ## Perfor
         merged.ol <- unique(rbind(ori.lift2.other.ol, other.lift2.ori.ol))
         
         out <- rep("X", length(original.peaks[[origin.species]])) ## All peaks are marked as X by default
-        out[which(width(list.of.compara[[origin.species]][[other.species]][[1]])==0)] = "D" ## Matched deletion regions are marked as D
+        out[which(sapply(width(list.of.compara[[origin.species]][[other.species]]), function(x) x[1] == 0) &
+                  sapply(seqnames(list.of.compara[[origin.species]][[other.species]]), function(x) x@values!="chr0"))] = "D"
+        ## Matched deletion regions are marked as D
         out[sum(width(list.of.compara[[origin.species]][[other.species]]) > 0) >=1] <- "0" ## Those that matching a non-dummy region are recorded as 0
         out[conserved.pks] <- sapply(conserved.pks, function(a.pk){ ## Those matching one or more regions have the full region(s) reported
             paste(unique(subset(merged.ol, queryHits==a.pk)$oripk), collapse=":")
